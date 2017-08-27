29ded6c477a2865c22e292caf1baf1c059a544bf
Ólafur Páll Geirsson <olafurpg@gmail.com>
2017-08-27 15:39:21 +0200

    Normalize conf after parsing.
    This teaches me I should have written more Scala.js tests. A simple
    config unit test would have caught it. Conf.normalize is the
    typesafe/config "resolve" equivalent.

--- scalafmt-core/js/src/main/scala/org/scalafmt/config/PlatformConfig.scala
+++ scalafmt-core/js/src/main/scala/org/scalafmt/config/PlatformConfig.scala
@@ -1,5 +1,13 @@
 package org.scalafmt.config
 
+import scala.meta.inputs.Input
+import metaconfig.Conf
+import metaconfig.Configured
+import metaconfig.MetaconfigParser
+
 object PlatformConfig {
-  implicit val parser = metaconfig.hocon.hoconMetaconfigParser
+  implicit val parser: MetaconfigParser = new MetaconfigParser {
+    override def fromInput(input: Input): Configured[Conf] =
+      metaconfig.hocon.hoconMetaconfigParser.fromInput(input).map(_.normalize)
+  }
 }
